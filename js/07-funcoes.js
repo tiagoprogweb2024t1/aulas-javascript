a381430a88a22212766f93f44f662675b8df379b
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-04-23 17:25:49 -0300

    Arrow Function e métodos

--- js/07-funcoes.js
+++ js/07-funcoes.js
@@ -58,15 +58,69 @@ let desconto = preco * 0.10; // 10%
 let precoFinal = preco - desconto;
 
 function formatarValor(valor){
-    return new Intl.NumberFormat("pt-br", {
+    /* Constante/variável de escopo LOCAL, ou seja,
+    só pode ser usada aqui dentro desta função. */
+    const opcoes = {
         style: "currency",
         currency: "BRL"
-    }).format(valor);
+    };
+
+    return new Intl.NumberFormat("pt-br", opcoes).format(valor);
 }
 
 console.log(`Preço: ${formatarValor(preco)}`);
 console.log(`Desconto: ${formatarValor(desconto)}`);
 console.log(`Preço final: ${formatarValor(precoFinal)}`);
+
+console.log("--------------");
+
+/* Sobre a sintaxe 3: Arrow Function
+Sintaxe moderna para funções no JavaScript, bastante
+usada em bibliotecas/frameworks como React, React Native,
+Angular, Vue e etc. 
+
+A sintaxe geral é:
+
+const algumNome = () => {};
+
+No entanto, esta sintaxe pode ser simplificada em alguns casos. */
+
+// Versão 1 (nomeada/declarada)
+/* function dobra(valor){
+    return valor * 2;
+} */
+
+// Versão 2 (arrow function)
+/* const dobra = (valor) => {
+    return valor * 2;
+}; */
+
+// Versão 3 (arrow function com retorno implícito)
+const dobra = valor => valor * 2;
+
+// Chamada
+console.log( dobra(10) );
+console.log( dobra(55) );
+console.log( dobra(1278.99) );
+
+console.log("-----------");
+
+// Funções como métodos/comportamentos de Objetos
+let pessoa = {
+    nome: "Fulano",
+    idade: 25,
+    verificaIdade(){
+        if(pessoa.idade >= 18){
+            return "maior";
+        } else {
+            return "menor"
+        }
+    }
+};
+console.log(pessoa.nome);
+console.log(`É ${pessoa.verificaIdade()}`);
+
+
 
 
 
