798ad4d9587ba860785f8d9975b45b1d1e286650
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-04-24 15:18:21 -0300

    Formatação

--- js/exercicio03.js
+++ js/exercicio03.js
@@ -7,5 +7,26 @@ function converterEmReais(valor){
     return valor * cotacaoDoDia;
 }
 
+function formatarMoeda(valor){
+    const opcoes = {
+        style: "currency",
+        currency: "BRL"
+    };
+
+    return new Intl.NumberFormat(
+        "pt-br", opcoes
+    ).format(valor);
+}
+
+// Versão sem formatação
 console.log( converterEmReais(valorEmDolar) );
+
+// Versão 1 (função como parâmetro para outra função) com formatação
+console.log( 
+    formatarMoeda( converterEmReais(valorEmDolar) ) 
+);
+
+// Versão 2 (primeiro, guardamos o resultado da conversão; depois passamos o resultado para a formatação)
+let resultadoEmReais = converterEmReais(valorEmDolar);
+console.log( formatarMoeda(resultadoEmReais) );
 
