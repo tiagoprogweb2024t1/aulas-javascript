df7747f66e0f47ebbf6b49a98525ed35e27e9f09
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-04-24 16:06:11 -0300

    Melhorias no desafio

--- js/exercicio03.js
+++ js/exercicio03.js
@@ -7,14 +7,14 @@ function converterEmReais(valor){
     return valor * cotacaoDoDia;
 }
 
-function formatarMoeda(valor, siglaDaMoeda){
+function formatarMoeda(valor, siglaDaMoeda, localizacao){
     const opcoes = {
         style: "currency",
         currency: siglaDaMoeda
     };
 
     return new Intl.NumberFormat(
-        "pt-br", opcoes
+        localizacao, opcoes
     ).format(valor);
 }
 
@@ -23,12 +23,12 @@ console.log( converterEmReais(valorEmDolar) );
 
 // Versão 1 (função como parâmetro para outra função) com formatação
 console.log( 
-    formatarMoeda( converterEmReais(valorEmDolar), "BRL" ) 
+    formatarMoeda( converterEmReais(valorEmDolar), "BRL", "pt-br" ) 
 );
 
 // Versão 2 (primeiro, guardamos o resultado da conversão; depois passamos o resultado para a formatação)
 let resultadoEmReais = converterEmReais(valorEmDolar);
-console.log( formatarMoeda(resultadoEmReais, "BRL") );
+console.log( formatarMoeda(resultadoEmReais, "BRL", "pt-br") );
 
 // Formatando o valor em dólar
-console.log( formatarMoeda(valorEmDolar, "USD") );
+console.log( formatarMoeda(valorEmDolar, "USD", "en") );
