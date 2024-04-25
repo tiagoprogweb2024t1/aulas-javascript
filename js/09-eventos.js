91eb5b0bbfff3f95bd29833dcbadd14d934a1b2c
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-04-25 16:27:12 -0300

    Video

--- js/09-eventos.js
+++ js/09-eventos.js
@@ -4,10 +4,21 @@
 const exemplo01 = document.querySelector("#exemplo01");
 const mensagem = document.querySelector("#mensagem");
 const pagina = document.querySelector("body");
+const video = document.querySelector("video");
 
 // Criando um objeto de áudio referenciando o arquivo
 const efeitoSonoro = new Audio();
 efeitoSonoro.src = "wow.wav";
+
+// Criando um objeto de imagem referenciando o arquivo
+const imagem = new Image();
+imagem.src = "wow.gif"; // ou meme.gif
+
+/* Se quiser criar referência de video via JS */
+// const video = document.createElement("video");
+// video.src = "pink-floyd.mp4";
+// video.width = 354;
+// video.height = 196;
 
 
 /* Criando um Ouvinte de Evento (Event Listener)
@@ -26,5 +37,12 @@ exemplo01.addEventListener("click", function(){
 
     // Executando o áudio
     efeitoSonoro.play();
+
+    // Inserindo a imagem após o parágrafo de mensagem
+    mensagem.insertAdjacentElement("afterend", imagem);    
+
+    // Remover o atributo hidden (isso fará o vídeo aparecer)
+    video.removeAttribute("hidden");
+    video.play(); // Reproduzir o video
 });
 
