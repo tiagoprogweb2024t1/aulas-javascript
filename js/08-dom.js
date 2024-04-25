6df8006e0df7f9eeed73f90437b41f75dbe7555c
tiagoprogweb2024t1 <tiago.progweb2024t1@gmail.com>
2024-04-25 14:20:29 -0300

    HTML com dados dinâmicos

--- js/08-dom.js
+++ js/08-dom.js
@@ -70,5 +70,24 @@ const listaDeEditores = [
         nome: "Xcode",
         finalidade: "Desenvolvimento de aplicações iOS"
     }
-]
+];
 
+// Acessando/selecionando a lista que será usada no HTML
+const lista = document.querySelector(".lista");
+console.log(lista);
+
+/* Percorrer (loop) o array de listaDeEditores,
+criar os itens <li> com o conteúdo do array e adicioná-los
+à lista HTML. */
+
+// for (variavel/constante de acesso of arrayComDadosQueQueremosAcessar) {}
+for(const editor of listaDeEditores){
+    // Etapa 1: criar elemento <li>
+    const item = document.createElement("li");
+    
+    // Etapa 2: adicionar conteúdo ao novo elemento
+    item.innerHTML = `<b>${editor.nome}</b>: ${editor.finalidade}`;
+    
+    // Etapa 3: adicionar o elemento à lista <ol>
+    lista.appendChild(item);
+}
